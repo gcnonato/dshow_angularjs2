002aaef15c967bfff7784181b9935ce40271791e
Murad Ramazanli <qargali@outlook.com>
2016-09-10 00:51:58 +0400

    this is needed

--- include/get.php
+++ include/get.php
@@ -2,10 +2,15 @@
     header('Content-type:text/javascript');
     $url = $_GET['data'];
     $lists = explode(',',$url);
+    $x=false;
+    if($x)
     $url ='http://dshow.herokuapp.com/include/getdatas.php?'.implode("&",$lists);
-
+    else
+    $url = "http://" .$_SERVER['SERVER_NAME']. "/include/getdatas.php?" .implode("&",$lists);
+    echo $url;
+    print_r($_SERVER);
     
-    echo file_get_contents($url);
+    //echo file_get_contents($url);
 
 
 ?>
